c2f35cca5f7dd73cae1695bd07a11167661cc094
briancalvet <briancalvet16@gmail.com>
2023-02-12 20:33:48 -0300

    Creamos el menu de jugador, tiempo y mejor puntaje

--- game.js
+++ game.js
@@ -5,16 +5,37 @@ const btnLeft = document.querySelector("#left");
 const btnRight = document.querySelector("#right"); 
 const btnDown = document.querySelector("#down"); 
 const listaBtn = document.querySelector(".btns"); 
+const spanLives = document.querySelector("#lives"); 
+const spanTime =  document.querySelector("#Time"); 
+const mejorTiempo = document.querySelector("#TiempoRecord")
+
 
 let canvasSize; 
 let elementsSize;
+let contadorNivel = 0; 
+let map = maps[0]; 
+let lives = 3; 
+let tiempoDeInicio; 
+let tiempoDeJugador;
+let intervaloDeTiempo; 
+let mejorTiempoJugador; 
 
-/*
+
 const playerPosition = {
-    x: undefined
-    y: undefined
+    x: undefined,
+    y: undefined,
 }
-*/
+
+const giftPosition = {
+
+    x: undefined,
+    y: undefined,
+
+}
+
+let enemiesPosition = [];
+
+
 window.addEventListener("load" , setCanvasSize);
 window.addEventListener("resize" , setCanvasSize);
 
@@ -33,7 +54,6 @@ function setCanvasSize(){
     canvas.setAttribute("height" , canvasSize); 
     
     elementsSize = canvasSize / 10; 
-
     startGame()
 
 }
@@ -44,32 +64,107 @@ function startGame(){
     game.font = elementsSize  + "px Verdana " ; 
     game.textAlign = "end"; 
 
+    if(!map){
+      gameWin(); 
+      return; 
+    }
 
-    let map = maps[0]; 
+    if(!tiempoDeInicio){
+        tiempoDeInicio = Date.now(); 
+        intervaloDeTiempo = setInterval(mostrarTiempo,100); 
+        mostrarRecord(); 
+    
+    }
+    
     const mapRows = map.trim().split("\n"); // al mapa 0 lo limpiamos le sacamos los espacion con trim, y el primer elemento que halla sea el \n
     // split crea un arreglo mediante dicho metodo.
     const mapRowsCols = mapRows.map(row => row.trim().split(""));
     // cada fila pasara a ser un arreglo y cada fila a su ves sera otro arreglo. 
- 
-    renderizarMapa(mapRowsCols)
+    console.log(({map,mapRows,mapRowsCols})); 
+
+    enemiesPosition = []; 
+    game.clearRect(0,0, canvasSize, canvasSize) // con este clearRect borramos todos lo renderizado hasta el momento
+    
+    
+
+    mostrarVidas();
+
+    mapRowsCols.forEach((row,rowI) => {
+        row.forEach((col,colI) =>{
+            const emoji = emojis[col];                  // buscamos renderizar cada columna y fila con los emojis correspondientes
+            const posX = elementsSize * (colI + 1); 
+            const posY = elementsSize * (rowI + 1); 
+
+            if(col == "O" && (!playerPosition.x && !playerPosition.y) ){ // lo que buscamos con este condicional es primero encontrar la posisicion de la calavera y en segunda instancia con el segundo condicional buscamos que las posiciones no sean las mismas que las que fueron declaradas en un principio
+                playerPosition.x = posX; 
+                playerPosition.y = posY;            // buscamos estableces la posicion de el jugador usando dos coordenadas x e y 
+                console.log({playerPosition});
+            } else if(col == "I"){
+                giftPosition.x = posX; 
+                giftPosition.y = posY; 
+            }else if (col == "X"){
+                enemiesPosition.push({
+                    x: posX,
+                    y: posY,
+
+                });
+            }
+
+            game.fillText(emoji, posX, posY); 
+        })
+    })
   
+    movePlayer();
+
+
+}
+
+function movePlayer(){
+   
+    const giftColisionX = giftPosition.x.toFixed(3) == playerPosition.x.toFixed(3); // comparamos las ubicaciones de x entre el regalo y el player (Usamos toFixed para reducir a 3 los decimales a contemplar)
+    const giftColisionY = giftPosition.y.toFixed(3) == playerPosition.y.toFixed(3); // comparamos las ubicaciones de y entre el regalo y el player. 
+    const huboColisionConElRegalor = giftColisionX && giftColisionY; 
+    
   
+    game.fillText(emojis["PLAYER"] , playerPosition.x , playerPosition.y); 
+   
+    
+    levelWin(huboColisionConElRegalor);
 
+   const enemyCollision= enemiesPosition.find(enemy => {
+      const enemyInX =  enemy.x.toFixed(3) == playerPosition.x.toFixed(3);  
+      const enemyInY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3); 
+      return enemyInX && enemyInY; 
+   } )
 
+   if(enemyCollision){
+    regresarPlayerPuertaYRestarVida()
+    explotarBombar() 
+   }
+
+  
+   
 }
 
-function renderizarMapa(mapa){
-    for(let row = 1; row <= 10; row++){
-        for(let col = 1; col <= 10; col++){
-            game.fillText(emojis[mapa[row - 1][col - 1]], elementsSize * col , elementsSize * row );
-            
-        } 
-      
-}
-}
+
 
 // escucha el evento del teclado -- keydown cuando baja la tecla 
 window.addEventListener("keydown" , moveByKeys )
+
+
+function mostrarVidas(){ // funcion que muestra las cantidad de vidas que tiene el jugador. 
+
+   const hearthsArray = Array(lives).fill(emojis["HEART"]) // [] se crea un array que no sabemos los elementos que tendra pero se creara a partir de la variable que le pasemos; 
+   
+    spanLives.innerHTML = ""; 
+    hearthsArray.forEach(hearth => spanLives.append(hearth))
+    
+
+
+}
+
+
+
 
 
 // escucha el evento de los botones de la consola.
@@ -78,23 +173,64 @@ btnLeft.addEventListener("click" ,  movimientoLeft);
 btnDown.addEventListener("click" ,  movimientoDown); 
 btnRight.addEventListener("click" ,  movimientoRight); 
 
+
+// funciones que var a determinar el movimiento del jugador. 
 function movimientoUp(){
-    console.log("Up");
+    
+    if((playerPosition.y - elementsSize) > 0){ // delimitamos el area de movimiento de nuestro player. 
+
+        console.log("Up");
+        playerPosition.y -= elementsSize; 
+        startGame();
+
+    }
+    
+   
+    
 }
 
 function movimientoLeft(){
-    console.log("Left");
+    if((playerPosition.x - elementsSize) > 0){
+
+        console.log("Left");
+        playerPosition.x -= elementsSize; 
+        startGame(); 
+    }
+        
+}
+
+function mostrarTiempo(){
+   spanTime.innerHTML = Date.now() - tiempoDeInicio; 
+   
+}
+
+function mostrarRecord(){
+mejorTiempo.innerHTML = localStorage.getItem("Record");  
 }
 
 function movimientoDown(){
-    console.log("Down");
+
+    if((playerPosition.y + elementsSize) < canvasSize){
+
+        console.log("Down");
+        playerPosition.y += elementsSize;
+        startGame(); 
+       
+    }
+
+  
 }
 
 function movimientoRight(){
-    console.log("Right"); 
+    
+    
+    if((playerPosition.x + elementsSize) < canvasSize){
+        console.log("Right"); 
+        playerPosition.x += elementsSize; 
+        startGame(); 
+    }
+    
 }
-
-
 
 
 function moveByKeys(btnSeleccionado){
@@ -112,8 +248,63 @@ function moveByKeys(btnSeleccionado){
 
 }
 
-function posicionarJugador(columna){
-    if (columna == "O"){
-        console.log("Aqui debe de ir el jugador")
+function levelWin(hayColision){
+    if (hayColision){
+        console.log("Subiste de nivel"); 
+        contadorNivel++;
+        map = maps[contadorNivel];
+        startGame(); 
     }
 }
+
+function gameWin(){
+   clearInterval(intervaloDeTiempo); 
+   guardarTiempo()
+}
+
+
+function regresarPlayerPuertaYRestarVida(){
+    if(lives <= 0) {
+       map = maps[0];
+       lives = 3;
+       tiempoDeInicio = undefined; 
+     
+        
+}
+    restarVida() 
+     
+    playerPosition.x = undefined; 
+    playerPosition.y = undefined;
+    startGame(); 
+
+} 
+
+function  explotarBombar(){
+    
+    game.fillText(emojis["BOMB_COLLISION"] , enemiesPosition.posX , enemiesPosition.posY);     
+
+}
+
+function restarVida(){
+    lives = lives - 1; 
+}
+
+function guardarTiempo(){
+    tiempoDeJugador = Date.now() - tiempoDeInicio; 
+    puntuacionMayor = localStorage.getItem("Record"); 
+   
+    if(puntuacionMayor){
+        if(puntuacionMayor > tiempoDeJugador){
+            localStorage.setItem("Record" , tiempoDeJugador )
+            console.log("Superaste el record anterior") 
+        }else {
+            console.log("No superaste el record. ")
+        }
+     }else {
+        localStorage.setItem("Record" , tiempoDeJugador )
+     }
+      
+    console.log({tiempoDeJugador , puntuacionMayor})
+}
+
+
